ec38294699ab63ec0cd99b3217871601cd5fe38d
BillyWei <across_horizon@qq.com>
2023-07-16 16:37:51 +0800

    update Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -71,7 +71,6 @@ public class Main {
     }
 
     public static void main(String[] args) throws Exception {
-        //testKV();
-        HexViewer.printFile( "/Users/bill/Downloads/user_data.kva", 320);
+        testKV();
     }
 }
